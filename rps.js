2e933d256e9e5c0f2d38095b345ed4d74d82276e
AJAHN1999 <tundlaajahn@gmail.com>
2023-12-29 19:25:49 +1100

    this commit contains major changes to code including buttons for interaction in the game and styling for a better look, however there are further refinements to be done both in styling and the code base

--- rps.js
+++ rps.js
@@ -1,20 +1,33 @@
 
 const ROUNDS = 5;
 function getComputerChoice() {
+    let textToDisplay;
     const options = [3, 1, 2]
     let choice = Math.floor(Math.random() * options.length);
     // get random item
+    
+    document.querySelector('.computerChoice').textContent=`Computer Choice:  ${convertChoice(choice)}`;
     let item = options[choice];
     return item;
 }
-
-function playerChoice() {
-    const player = parseInt(prompt("enter your choice:\n1)Paper\n2)Scissors\n3)Rock"));
-    if (player > 3 || player < 1) {
-        playerChoice()
+function convertChoice(choice){
+    if(choice===1){
+        textToDisplay='Paper';
+        return textToDisplay;
     }
-    return player
+    else if(choice==2){
+        textToDisplay='Scissors';
+        return textToDisplay;
+    }
+    else{
+        textToDisplay='Rock';
+        return textToDisplay;
+    }
 }
+
+function playerChoice(event) {
+    return event.target.value;
+ }
 
 function logic(pS, cS) {
     let pScore = 0;
@@ -30,37 +43,75 @@ function logic(pS, cS) {
     return [pScore, cScore, tieScore]
 }
 
-function playRound(playerSelection, computerSelection) {
-    //let playerFinalScore=0;
-    //let computerFinalScore=0;
+let playerFinalScore=0;
+let computerFinalScore=0;
+
+function playRound(playerSelection, computerSelection) { 
     console.log('PLAYER SELECTION IS ' + playerSelection)
     console.log('COMPUTER SELECTION IS ' + computerSelection)
     let scores = logic(playerSelection, computerSelection);
     playerScore = scores[0]
     computerScore = scores[1]
     tieScores = scores[2]
-    // playerFinalScore+=playerScore;
-    // computerFinalScore+=computerScore;
+    playerFinalScore+=playerScore;
+    computerFinalScore+=computerScore;
+    document.querySelector('.scoreKeeper').textContent=`Player:${playerFinalScore} - Computer:${computerFinalScore}`;
     return [playerScore, computerScore, tieScores]
 }
+prompts=['rock!!','paper!!','scissors!!'];
 
 
-function game() {
+function displayPrompt(){
+index=0;
+const rpsPrompt = document.querySelector('.rpsPrompt');
+rpsPrompt.textContent=prompts[index];
+prompts.splice(index,1);
+if(prompts.length!=0){setTimeout(displayPrompt,1000)}
+else{prompts=['rock!!','paper!!','scissors!!'];}
+};
 
-    for (i = 0; i < ROUNDS; i++) {
+
+
+
+function game(number) {
         let computerSelection = getComputerChoice();
-        let playerSelection = playerChoice();
+        let playerSelection=number;
+        document.querySelector('.playerChoice').textContent=`Player Choice:  ${convertChoice(number)}`
+        //if(isNaN(playerSelection)){console.log('player choice is Nan');}
+        if(playerFinalScore==5||computerFinalScore==5){
+            if(playerFinalScore==5){
+                playerFinalScore=0;
+                computerFinalScore=0;
+                document.querySelector('#message').textContent='Player wins the game!!';
+                return;
+            }
+            else if(computerFinalScore==5){
+                computerFinalScore=0;
+                playerFinalScore=0;
+                document.querySelector('#message').textContent='Computer wins the game!!';
+                return;}
+        }
         let result = playRound(playerSelection, computerSelection);
         if (result[2] == 1) {
-            i--
+            document.querySelector('#message').textContent='it is a draw';
             console.log('it is a draw')
         }
-        else if (result[0] > result[1]) { console.log('player wins') }
-        else { console.log('computer wins') }
-    }
+        else if (result[0] > result[1]) { console.log('player wins') 
+        document.querySelector('#message').textContent='player wins';}
+        else { console.log('computer wins') 
+        document.querySelector('#message').textContent='computer wins';}
 }
 
 
 
+function startGame(){
+    document.querySelector('.playButton').addEventListener('click',()=>{
+        displayPrompt();
+        document.querySelector('.rockButton').addEventListener('click',()=>{game(3)});
+        document.querySelector('.paperButton').addEventListener('click',()=>{game(1)});
+        document.querySelector('.scissorsButton').addEventListener('click',()=>{game(2)});
 
-game();
+    })
+}
+
+startGame();
